c771794d8934deeee8c9632ab38424e4abd5c1b5
israel david dahan <id@STUDIODVD.CO.IL>
2022-12-07 19:09:24 +0200

    tezt Input

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,6 +3,6 @@ import React from "react";
 function Input(props) {
   const changeHandler = props.onChange;
 
-  return <input type="text" />;
+  return <input type="text" onChange={changeHandler}/>;
 }
 export default Input;
